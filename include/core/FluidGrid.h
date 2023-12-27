d212b8e6960e3cc9382e6cd50ca254783cb66db8
infopek <10saero01@gmail.com>
2023-12-27 23:21:23 +0100

    Implement FluidGrid

--- include/core/FluidGrid.h
+++ include/core/FluidGrid.h
@@ -1,17 +1,54 @@
 #pragma once
 
-#include "Vec2.h"
-
 #include <vector>
 
-/*
-* Represents an Eulerian grid for fluid simulation.
+/**
+ * Represents an Eulerian grid for fluid simulation.
+ * This implementation uses a staggering grid.
 */
 class FluidGrid
 {
 public:
-    FluidGrid();
+    FluidGrid(size_t width, size_t height);
+
+    size_t getWidth() const { return m_width; }
+    size_t getHeight() const { return m_height; }
+
+    /// @return The horizontal component of the velocity vector at `[row, col]`
+    /// if `row` and `col` are within bounds, `0` otherwise (wall).
+    float getU(size_t row, size_t col) const;
+
+    /// Sets the horizontal component of the velocity vector at `[row, col]`
+    /// if `row` and `col` are within bounds.
+    void setU(size_t row, size_t col, float newU);
+
+    /// @return The vertical component of the velocity vector at `[row, col]`
+    /// if `row` and `col` are within bounds, `0` otherwise (wall).
+    float getV(size_t row, size_t col) const;
+
+    /// Sets the vertical component of the velocity vector at `[row, col]`
+    /// if `row` and `col` are within bounds.
+    void setV(size_t row, size_t col, float newV);
+
+    /// @return `1` if the cell at `[row, col]` is a fluid cell, `0` otherwise (wall).
+    /// Out of bounds access is treated as wall.
+    uint8_t getS(size_t row, size_t col) const;
+
+    /// Set the cell state at `[row, col]` to fluid cell (`1`) or wall (`0`).
+    void setS(size_t row, size_t col, uint8_t newS);
+
+    float getP(size_t row, size_t col) const { return m_p[row * m_width + col]; }
+
+    void setP(size_t row, size_t col, float newP) { m_p[row * m_width + col] = newP; }
 
 private:
-    std::vector<Vec2[4]> m_grid;    // staggered grid
+    std::vector<float> m_u{};   // horizontal components of velocity vectors
+    std::vector<float> m_v{};   // vertical components of velocity vectors
+
+    std::vector<uint8_t> m_s{};   // 0 if wall, 1 if fluid cell
+
+    std::vector<float> m_p{};   // pressure field
+
+    size_t m_width{};
+    size_t m_height{};
 };
--- src/core/FluidGrid.cpp
+++ src/core/FluidGrid.cpp
@@ -1,1 +1,51 @@
 #include "core/FluidGrid.h"
+
+FluidGrid::FluidGrid(size_t width, size_t height)
+    : m_width{ width }, m_height{ height },
+    m_u(width* height, 0.0f),
+    m_v(width* height, 0.0f),
+    m_s(width* height, 1),
+    m_p(width* height, 0.0f)
+{
+}
+
+float FluidGrid::getU(size_t row, size_t col) const
+{
+    return row < m_height && col < m_width
+        ? m_u[row * m_width + col]
+        : 0.0f;
+}
+
+void FluidGrid::setU(size_t row, size_t col, float newU)
+{
+    if (row >= m_height || col >= m_width)
+        return; // wall doesn't change
+
+    m_u[row * m_width + col] = newU;
+}
+
+float FluidGrid::getV(size_t row, size_t col) const
+{
+    return row < m_height && col < m_width
+        ? m_v[row * m_width + col]
+        : 0.0f;
+}
+
+void FluidGrid::setV(size_t row, size_t col, float newV)
+{
+}
+
+uint8_t FluidGrid::getS(size_t row, size_t col) const
+{
+    return row < m_height && col < m_width
+        ? m_s[row * m_width + col]
+        : 0;
+}
+
+void FluidGrid::setS(size_t row, size_t col, uint8_t newS)
+{
+    if (row >= m_height || col >= m_width)
+        return;
+
+    m_s[row * m_width + col] = newS;
+}
