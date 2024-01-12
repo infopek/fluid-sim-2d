28b5ddb0566c984cd271aa6acbb4dcc9711cd637
infopek <10saero01@gmail.com>
2024-01-12 19:32:38 +0100

    Create a 'next' field for every grid

--- include/core/FluidGrid.h
+++ include/core/FluidGrid.h
@@ -11,8 +11,33 @@ class FluidGrid
 public:
     FluidGrid(size_t width, size_t height);
 
+    /// Sets the current horizontal component field to the next horizontal component field.
+    void updateU();
+
+    /// Sets the current vertical component field to the next vertical component field.
+    void updateV();
+
+    /// Sets the next horizontal component field to the current horizontal component field.
+    void updateNextU();
+
+    /// Sets the next vertical component field to the current vertical component field.
+    void updateNextV();
+
+    void updateM();
+
+    void updateNextM();
+
+    /* --------------- GETTERS & SETTERS --------------- */
+
     size_t getWidth() const { return m_width; }
+
     size_t getHeight() const { return m_height; }
+
+    const std::vector<float>& getUGrid() const { return m_u; }
+
+    const std::vector<float>& getVGrid() const { return m_v; }
+
+    const std::vector<float>& getMGrid() const { return m_m; }
 
     /// @return The horizontal component of the velocity vector at `[row, col]`
     /// if `row` and `col` are within bounds, `0` otherwise (wall).
@@ -30,6 +55,30 @@ public:
     /// if `row` and `col` are within bounds.
     void setV(size_t row, size_t col, float newV);
 
+    float getM(size_t row, size_t col) const;
+
+    void setM(size_t row, size_t col, float newM);
+
+    /// @return The horizontal component of the next velocity vector at `[row, col]`
+    /// if `row` and `col` are within bounds, `0` otherwise (wall).
+    float getNextU(size_t row, size_t col) const;
+
+    /// Sets the horizontal component of the next velocity vector at `[row, col]`
+    /// if `row` and `col` are within bounds.
+    void setNextU(size_t row, size_t col, float newU);
+
+    /// @return The vertical component of the next velocity vector at `[row, col]`
+    /// if `row` and `col` are within bounds, `0` otherwise (wall).
+    float getNextV(size_t row, size_t col) const;
+
+    /// Sets the vertical component of the next velocity vector at `[row, col]`
+    /// if `row` and `col` are within bounds.
+    void setNextV(size_t row, size_t col, float newV);
+
+    float getNextM(size_t row, size_t col) const;
+
+    void setNextM(size_t row, size_t col, float newM);
+
     /// @return `1` if the cell at `[row, col]` is a fluid cell, `0` otherwise (wall).
     /// Out of bounds access is treated as wall.
     uint8_t getS(size_t row, size_t col) const;
@@ -44,6 +93,11 @@ public:
 private:
     std::vector<float> m_u{};   // horizontal components of velocity vectors
     std::vector<float> m_v{};   // vertical components of velocity vectors
+    std::vector<float> m_m{};
+
+    std::vector<float> m_nextU{};   // updated horizontal components of velocity vectors
+    std::vector<float> m_nextV{};   // updated vertical components of velocity vectors
+    std::vector<float> m_nextM{};
 
     std::vector<uint8_t> m_s{};   // 0 if wall, 1 if fluid cell
 
--- src/core/FluidGrid.cpp
+++ src/core/FluidGrid.cpp
@@ -1,51 +1,153 @@
 #include "core/FluidGrid.h"
 
+#include <cstdint>
+
 FluidGrid::FluidGrid(size_t width, size_t height)
-    : m_width{ width }, m_height{ height },
-    m_u(width* height, 0.0f),
-    m_v(width* height, 0.0f),
-    m_s(width* height, 1),
-    m_p(width* height, 0.0f)
+	: m_width{ width }, m_height{ height },
+	m_u((width + 2)* (height + 2), 0.0f),
+	m_v((width + 2)* (height + 2), 0.0f),
+	m_m((width + 2)* (height + 2), 1.0f),
+	m_nextU((width + 2)* (height + 2), 0.0f),
+	m_nextV((width + 2)* (height + 2), 0.0f),
+	m_nextM((width + 2)* (height + 2), 0.0f),
+	m_s((width + 2)* (height + 2), 1),
+	m_p((width + 2)* (height + 2), 0.0f)
 {
+	m_width += 2;
+	m_height += 2;
+}
+
+void FluidGrid::updateU()
+{
+	m_u = m_nextU;
+}
+
+void FluidGrid::updateV()
+{
+	m_v = m_nextV;
+}
+
+void FluidGrid::updateNextU()
+{
+	m_nextU = m_u;
+}
+
+void FluidGrid::updateNextV()
+{
+	m_nextV = m_v;
+}
+
+void FluidGrid::updateM()
+{
+	m_m = m_nextM;
+}
+
+void FluidGrid::updateNextM()
+{
+	m_nextM = m_m;
 }
 
 float FluidGrid::getU(size_t row, size_t col) const
 {
-    return row < m_height && col < m_width
-        ? m_u[row * m_width + col]
-        : 0.0f;
+	return row < m_height && col < m_width
+		? m_u[row * m_width + col]
+		: 0.0f;
 }
 
 void FluidGrid::setU(size_t row, size_t col, float newU)
 {
-    if (row >= m_height || col >= m_width)
-        return; // wall doesn't change
+	if (row >= m_height || col >= m_width)
+		return; // wall doesn't change
 
-    m_u[row * m_width + col] = newU;
+	m_u[row * m_width + col] = newU;
 }
 
 float FluidGrid::getV(size_t row, size_t col) const
 {
-    return row < m_height && col < m_width
-        ? m_v[row * m_width + col]
-        : 0.0f;
+	return row < m_height && col < m_width
+		? m_v[row * m_width + col]
+		: 0.0f;
 }
 
 void FluidGrid::setV(size_t row, size_t col, float newV)
 {
+	if (row >= m_height || col >= m_width)
+		return; // wall doesn't change
+
+	m_v[row * m_width + col] = newV;
+}
+
+float FluidGrid::getNextU(size_t row, size_t col) const
+{
+	return row < m_height && col < m_width
+		? m_nextU[row * m_width + col]
+		: 0.0f;
+}
+
+void FluidGrid::setNextU(size_t row, size_t col, float newU)
+{
+	if (row >= m_height || col >= m_width)
+		return; // wall doesn't change
+
+	m_nextU[row * m_width + col] = newU;
+}
+
+float FluidGrid::getNextV(size_t row, size_t col) const
+{
+	return row < m_height && col < m_width
+		? m_nextV[row * m_width + col]
+		: 0.0f;
+}
+
+void FluidGrid::setNextV(size_t row, size_t col, float newV)
+{
+	if (row >= m_height || col >= m_width)
+		return; // wall doesn't change
+
+	m_nextV[row * m_width + col] = newV;
+}
+
+float FluidGrid::getNextM(size_t row, size_t col) const
+{
+	return row < m_height && col < m_width
+		? m_nextM[row * m_width + col]
+		: 0.0f;
+}
+
+void FluidGrid::setNextM(size_t row, size_t col, float newM)
+{
+	if (row >= m_height || col >= m_width)
+		return; // wall doesn't change
+
+	m_nextM[row * m_width + col] = newM;
 }
 
 uint8_t FluidGrid::getS(size_t row, size_t col) const
 {
-    return row < m_height && col < m_width
-        ? m_s[row * m_width + col]
-        : 0;
+	return row < m_height && col < m_width
+		? m_s[row * m_width + col]
+		: 0;
 }
 
 void FluidGrid::setS(size_t row, size_t col, uint8_t newS)
 {
-    if (row >= m_height || col >= m_width)
-        return;
+	if (row >= m_height || col >= m_width)
+		return;
 
-    m_s[row * m_width + col] = newS;
+	m_s[row * m_width + col] = newS;
+}
+
+float FluidGrid::getM(size_t row, size_t col) const
+{
+	return row < m_height && col < m_width
+		? m_m[row * m_width + col]
+		: 0.0f;
+}
+
+void FluidGrid::setM(size_t row, size_t col, float newM)
+{
+	if (row >= m_height || col >= m_width)
+		return;
+
+	m_m[row * m_width + col] = newM;
 }
